7848896638f239982fcd28f891c71ceeb1575287
anargasimov1 <anar.gasimov.89@gmail.com>
2023-10-26 20:29:29 +0400

    v1.2

--- src/lesson/Lesson.java
+++ src/lesson/Lesson.java
@@ -1,75 +1,1 @@
-package lesson;
-import java.util.Scanner;
-
-public class Lesson {
-
-    private String username = "Anar";
-    private String parol = "123Anar";
-    private int[] crediLimit = {2350, 3500, 6200, 5400};
-    private String[] users = {"Anar", "Aysel", "Hesen", "Elnur"};
-
-    public static void main(String[] args) {
-
-        Lesson main = new Lesson();
-        main.logIn(main.parol, main.username,main.users, main.crediLimit);
-    }
-
-    public void logIn(String parol, String username,String[] users,int[] crediLimit ) {
-        System.out.println("istifadeci adinizi daxil edin");
-        byte chekin =0;
-        while (true) {
-            Scanner sc = new Scanner(System.in);
-            String chek = sc.nextLine();
-
-            if (username.equals(chek)) {
-                System.out.println("parolnuzu daxil edin");
-                String password = sc.nextLine();
-                if (parol.equals(password)) {
-                    chekin++;
-                    System.out.println("xos gelniniz!");
-                    lookUsers(users, crediLimit);
-                }
-                else System.out.println("parolunuz duzgun deyil");
-
-            } else {
-                System.out.println("istfadeci adiniz duzgun deyil");
-                chekin++;
-            }
-            if(chekin == 3){
-                System.exit(3);
-            }
-        }
-    }
-
-
-    public void lookUsers(String[] users, int[] crediLimit) {
-        for (int i = 0; i < users.length; i++) {
-            System.out.println((i + 1) + "." + users[i] + crediLimit[i] + " AZN");
-        }
-        prosseing();
-    }
-
-   public void prosseing(){
-       Scanner nm = new Scanner(System.in);
-       while (true) {
-           byte number = nm.nextByte();
-           if (number < users.length) {
-               System.out.println(users[number - 1] + crediLimit[number - 1]);
-               System.out.println("ne qeder mebleg istiyirsiz?");
-               int fiat = nm.nextInt();
-               if(fiat<crediLimit[number-1]){
-                   int newfiat = crediLimit[number - 1] - fiat;
-                   System.out.println("sizin " + newfiat + " mebleginiz qaldi");
-               }
-              else System.out.println("limiti asdiniz!");
-
-           }
-           else System.out.println("bele istfadeci yoxdur");
-           System.out.println("sistemden cixmaq ucun q duymesini secin ");
-           Scanner exit = new Scanner(System.in);
-           String quit = exit.nextLine();
-           if(quit.equals("q")) System.exit(0);
-       }
-
-   }
-}
+package lesson;import java.util.Scanner;public class Lesson {    private String[] username = new String[20];    private String[] parol = new String[20];    final private int[] crediLimit = {2350, 3500, 6200, 5400};    final private String[] users = {"Anar", "Aysel", "Hesen", "Elnur"};    public static void main(String[] args) {        Lesson main = new Lesson();        main.SingUp(main.parol, main.username);    }    public void SingUp(String[] parol, String[] username){        System.out.println("istifadeci adi teyin edin");        Scanner name = new Scanner(System.in);        String newuser = name.nextLine();        for (int i = 0; i < parol.length; i++) {            parol[i] = newuser;        }        System.out.println("parol teyin edin");        String password = name.nextLine();        for (int i = 0; i < username.length; i++) {            username[i] = password;        }           logIn(users, crediLimit, newuser, password);    }    public void logIn(String[] users,int[] crediLimit, String newuser, String password1 ) {        System.out.println("istifadeci adinizi daxil edin");        byte chekin =0;        while (true) {            Scanner sc = new Scanner(System.in);            String chek = sc.nextLine();            if (newuser.equals(chek)) {                System.out.println("parolnuzu daxil edin");                String password = sc.nextLine();                if (password1.equals(password)) {                    chekin++;                    System.out.println("xos gelniniz!");                    lookUsers(users, crediLimit);                }                else System.out.println("parolunuz duzgun deyil");            } else {                System.out.println("istfadeci adiniz duzgun deyil");                chekin++;            }            if(chekin == 3){                System.exit(3);            }        }    }    public void lookUsers(String[] users, int[] creditLimit) {        for (int i = 0; i < users.length; i++) {            System.out.println((i + 1) + "." + users[i] + creditLimit[i] + " AZN");        }        prosseing();    }   public void prosseing(){       Scanner nm = new Scanner(System.in);       while (true) {           byte number = nm.nextByte();           if (number < users.length) {               System.out.println(users[number - 1] + crediLimit[number - 1]);               System.out.println("ne qeder mebleg istiyirsiz?");               int fiat = nm.nextInt();               if(fiat<crediLimit[number-1]){                   int newfiat = crediLimit[number - 1] - fiat;                   System.out.println("sizin " + newfiat + " mebleginiz qaldi");               }              else System.out.println("limiti asdiniz!");           }           else System.out.println("bele istfadeci yoxdur");           System.out.println("sistemden cixmaq ucun 0 duymesini secin ");           if(number ==0) System.exit(0);       }   }}
